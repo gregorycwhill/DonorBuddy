261cf655579b012ad8eaf4e004b39d2b2366ccfb
gregorycwhill <gregory.hill@gmail.com>
2022-12-09 21:57:50 +1100

    Refactored forms
    Added SlimSelect, organisation, keywords and avoid filters.

--- js/main.js
+++ js/main.js
@@ -11,32 +11,75 @@ const subbtn = document.getElementById("btn-submit");
 
 $(document).ready(prefillForm);
 
-var opts=[['Prefer','Avoid'],['Places','Goals','Beneficiaries']];
-//opts[0] = ['Prefer','Avoid'];
-//opts[1] = ['Places','Goals','Beneficiaries'];
+var opts=[['Prefer','Avoid'],['Organisation','Places','Goals','Beneficiaries', 'Keywords']];
 
 SS={};
 
+var ssObj = {};
+
 for (var i in opts[0]) {
 	for (var j in opts[1]) {
-		SS[opts[0][i]+opts[1][j]] = new SlimSelect({
+		ssObj = {
 			select: '#'+opts[0][i].toLowerCase()+opts[1][j],
 			events: {
 				afterChange: (newVal) => {
 					swapOptions(newVal)
-					}
+					},
 			},
 			settings: {
-				placeholderText: opts[0][i]+' these '+opts[1][j],
+				placeholderText: opts[0][i]+' these '+opts[1][j].replace('Organisation','Organisation types'),	// slight tweak for Organisation
 				allowDeselect: true,
 				closeOnSelect: false,
 				showOptionTooltips: true,
-
 			}
-		})
+		};
+		if (opts[1][j]=='Keywords') {
+			// for Keywords selector, let users add the new keyword
+			// unless it already exists, to avoid duplicates
+			Object.assign(ssObj['events'],{
+				addable: function (val) { 
+					var tag=val.toLowerCase().replace(/\s/ig,'')
+					var vals=[];
+					var data=[];
+					for (var i in opts[0]) {
+						data = SS[opts[0][i]+'Keywords'].getData();
+						for (d in data)
+							vals.push(data[d].value);
+					}
+					
+					if (vals.includes(tag))
+						return false;
+				
+				// add to the complementary select list
+					curr=0;
+					if (document.getElementsByClassName('ss-main')[8].classList.length==4)		// CSS hacking to find out which Keyword Selector we're working with
+						comp=1;
+
+					SS[opts[0][comp]+'Keywords'].addOption({text: val, value: tag});
+
+					return tag;
+				},
+				beforeClose: () => { 							// remove the null entries introduced
+					for(var i in opts[0]) { 
+						var data=SS[opts[0][i]+'Keywords'].getData(); 
+						SS[opts[0][i]+'Keywords'].setData(data.filter((x)=>{return x.value!='';}));
+					}
+				}
+			});
+			
+			Object.assign(ssObj['settings'], {
+				searchText:	'No keywords yet',
+				searchPlaceholder: 'Enter keyword here and click + button on right'
+			});
+		}
+		
+		SS[opts[0][i]+opts[1][j]] = new SlimSelect(ssObj);
 	}
 	
 }
+
+
+
 
 formElem.addEventListener("submit", (e) => {
 
@@ -130,9 +173,12 @@ function swapOptions(newVal) {
 	
 	// Use the shorter value for the tags in the multiselect
 	// Enforce the logic that a tag can't be in both prefer and avoid list
-	
+
 	var id;
 	var tags=document.getElementsByClassName('ss-value');
+	
+	//console.log(newVal);
+	//console.log(tags);
 	
 	if (newVal.length==0)
 		return;
@@ -158,6 +204,8 @@ function swapOptions(newVal) {
 	var compSet = SS[comp].getSelected();
 	var currSet = SS[curr].getSelected();
 	
+	// Enforce the logic that a tag can't be in both prefer and avoid list
+	
 	if (currSet.length==0 || compSet.length==0)
 		return;
 	
@@ -167,6 +215,7 @@ function swapOptions(newVal) {
 		if (!currSet.includes(compSet[i]))
 			newCompSet.push(compSet[i]);
 	
+	//console.log(newCompSet);
 	SS[comp].setSelected(newCompSet);
 	
 	return;
